84f1fcc5a4a281f24b24781c608abe2113c36465
Altan <suyanovaltan@gmail.com>
2023-07-16 15:48:15 +0300

    getCargo and getCalculation works

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -1,6 +1,7 @@
 import getAuthToken from './getAuthToken'
+import getCalculation from './getCalculation';
 import getRefreshedAuthToken from './getRefreshedAuthToken'
-
+import getCargo from './getCargo';
 // getAuthToken('suyanovaltan@gmail.com', 'bqSJt2wpgA').then(data => console.log(data))
 
 let token: string = '';
@@ -10,7 +11,7 @@ let token: string = '';
 
 
 
-getAuthToken('suyanovaltan@gmail.com', 'bqSJt2wpgA').then(data => token = data.refresh_token).then(data => console.log(token)).then(data => getRefreshedAuthToken(token).then(data => console.log(data)))
+getAuthToken('suyanovaltan@gmail.com', 'bqSJt2wpgA').then(data => token = data).then(data => console.log(token)).then(data => getCargo(token).then(data => console.log(data)))
 // getRefreshedAuthToken('67e279dc1f7a9bdc621bebd5c2fec87fb0ed35d6').then(data => console.log(data))
 
 
