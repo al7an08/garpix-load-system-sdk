f9512980a07150acc6b494de3fe3f42d8f6e44f6
Altan <suyanovaltan@gmail.com>
2023-07-17 18:12:39 +0300

    update of imports

--- src/garpix-load-system-sdk.ts
+++ src/garpix-load-system-sdk.ts
@@ -1,3 +1,30 @@
 import getAuthToken from './lib/getAuthToken'
+import createNewCalculation from './lib/createNewCalculation'
+import deleteCargoById from './lib/deleteCargoById'
+import getCalculation from './lib/getCalculation'
+import getCargo from './lib/getCargo'
+import getCargoInfoById from './lib/getCargoInfoById'
+import getRefreshedAuthToken from './lib/getRefreshedAuthToken'
+import updateCargoById from './lib/updateCargoById'
 
+export {
+    getAuthToken,
+    createNewCalculation,
+    deleteCargoById,
+    getCalculation,
+    getCargo,
+    getCargoInfoById,
+    getRefreshedAuthToken,
+    updateCargoById
+}
 
+export default {
+    getAuthToken,
+    createNewCalculation,
+    deleteCargoById,
+    getCalculation,
+    getCargo,
+    getCargoInfoById,
+    getRefreshedAuthToken,
+    updateCargoById
+}
